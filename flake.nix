7fda066ef2e7659a2d73bb9cc5d979a25b7c7cfe
Anillc <i@anillc.cn>
2025-02-18 11:24:29 +0800

    add asciidoctor

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  outputs = inputs@{
+    self, nixpkgs, flake-parts,
+  }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    perSystem = { pkgs, ... }: let
+      asciidoctor = pkgs.callPackage ./asciidoctor {};
+      gen = pkgs.writeScriptBin "gen" ''
+        #!${pkgs.runtimeShell}
+        export PATH=$PATH:${pkgs.lib.makeBinPath (with pkgs; [ ruby bundix ])}
+        cd $MANUAL_PATH/asciidoctor
+        bundle lock --update prawn
+        bundix
+      '';
+      prawn-subtree = pkgs.writeScriptBin "prawn-subtree" ''
+        #!${pkgs.runtimeShell}
+        export PATH=$PATH:${pkgs.lib.makeBinPath (with pkgs; [ git ])}
+        cd $MANUAL_PATH
+        git subtree --prefix=asciidoctor/prawn "$@"
+      '';
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [];
+        nativeBuildInputs = with pkgs; [
+          asciidoctor gen prawn-subtree
+        ];
+        shellHook = ''
+          export MANUAL_PATH=$PWD
+        '';
+      };
+    };
+  };
+}
