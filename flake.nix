824f83ab1a8bb5c2fe7a7a226423ad1023187983
Anillc <i@anillc.cn>
2025-02-19 20:58:29 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -3,23 +3,24 @@
   outputs = inputs@{
     self, nixpkgs, flake-parts,
   }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    perSystem = { pkgs, ... }: let
+    systems = [ "x86_64-linux" ];
+    perSystem = { pkgs, self', ... }: let
       asciidoctor = pkgs.callPackage ./asciidoctor {};
       gen = pkgs.writeScriptBin "gen" ''
         #!${pkgs.runtimeShell}
         export PATH=$PATH:${pkgs.lib.makeBinPath (with pkgs; [ ruby bundix ])}
-        cd $MANUAL_PATH/asciidoctor
+        cd $SHELL_PATH/asciidoctor
         bundle lock --update prawn
         bundix
       '';
       subtrees = map (name: pkgs.writeScriptBin "${name}-subtree" ''
         #!${pkgs.runtimeShell}
         export PATH=$PATH:${pkgs.lib.makeBinPath (with pkgs; [ git ])}
-        cd $MANUAL_PATH
+        cd $SHELL_PATH
         git subtree --prefix=asciidoctor/${name} "$@"
       '') [ "prawn" "asciidoctor-pdf" ];
     in {
+      packages.default = asciidoctor;
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [];
         nativeBuildInputs = with pkgs; [
@@ -27,7 +28,7 @@
           nodePackages.wavedrom-cli
         ] ++ subtrees;
         shellHook = ''
-          export MANUAL_PATH=$PWD
+          export SHELL_PATH=$PWD
         '';
       };
     };
